dfbb120bcd88b2346532ff07b68b1e2fac1a6526
Juno <jvmeifert@gmail.com>
2024-12-03 19:28:12 -0500

    Fix volume remover utility

--- software/wazuh-server/rm-volumes.sh
+++ software/wazuh-server/rm-volumes.sh
@@ -2,4 +2,4 @@
 
 # This script removes all Docker volumes created by the lab.
 
-docker volume rm wazuh_wazuh_api_configuration wazuh_wazuh_etc wazuh_wazuh_logs wazuh_wazuh_queue wazuh_wazuh_var_multigroups wazuh_wazuh_integrations wazuh_wazuh_active_response wazuh_wazuh_agentless wazuh_wazuh_wodles wazuh_filebeat_etc wazuh_filebeat_var wazuh_wazuh-indexer-data wazuh_wazuh-dashboard-config wazuh_wazuh-dashboard-custom
+docker volume rm wazuh-server_wazuh_api_configuration wazuh-server_wazuh_etc wazuh-server_wazuh_logs wazuh-server_wazuh_queue wazuh-server_wazuh_var_multigroups wazuh-server_wazuh_integrations wazuh-server_wazuh_active_response wazuh-server_wazuh_agentless wazuh-server_wazuh_wodles wazuh-server_filebeat_etc wazuh-server_filebeat_var wazuh-server_wazuh-indexer-data wazuh-server_wazuh-dashboard-config wazuh-server_wazuh-dashboard-custom
