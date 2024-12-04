8ecd7519a6a5a67fb94336a42e4fbc48d445adb5
Juno <jvmeifert@gmail.com>
2024-12-03 23:09:53 -0500

    Install curl for those who do not have it

--- software/wazuh-agent/install.sh
+++ software/wazuh-agent/install.sh
@@ -2,6 +2,8 @@
 
 # This script configures the Wazuh repository and installs the Wazuh agent.
 
+apt install -y curl
+
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
